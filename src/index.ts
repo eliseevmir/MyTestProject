b2ea080a12cf359f8cd517786af44a39a7f6ab9b
Vladimir <eliseevmir@gmail.com>
2023-04-25 15:39:35 +0500

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,50 @@
 import http from "node:http";
+import url from "url";
 import { graphql } from "graphql";
-
 import schema from "./schema";
 
-const server = http.createServer();
+const graphqlHTTP = async (
+  req: http.IncomingMessage,
+  res: http.ServerResponse
+) => {
+  const params = url.parse(req.url || "", true);
 
-server.on("request", async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader("content-type", "application/json");
+  const query = params.query.q;
 
-  // const responce = JSON.stringify({
-  //   foo: "ok",
-  // });
+  const result: any = {
+    errors: [],
+    data: null,
+    extensions: null,
+  };
 
-  // res.write(responce);
+  if (typeof query === "string") {
+    const { errors, data, extensions } = await graphql({
+      schema,
+      source: query,
+    });
 
-  const d = /* GraphQl */ `
-    query Test ($name: String! $userId: ID!) {
-      hello(word: $name) 
-      user (userId: $userId) {
-        id
-        name
-        age
-      }
+    result.errors = errors;
+    result.data = data;
+    result.extensions = extensions;
   }
-  `;
 
-  const a = await graphql({
-    schema,
-    source: d,
-    variableValues: { name: "Ivan", userId: 123 },
-    contextValue: {foo: 'bar'}
+  return result;
+};
+
+const bootstrap = async () => {
+  const server = http.createServer();
+
+  server.on("request", async (req, res) => {
+    const { errors, data, extensions } = await graphqlHTTP(req, res);
+
+    res.statusCode = 200;
+    res.setHeader("content-type", "application/json");
+    res.write(JSON.stringify({ errors, data, extensions }));
+
+    return res.end();
   });
-  console.log(a);
 
-  res.write(JSON.stringify({ foo: "bar" }));
-  res.end();
-});
+  server.listen(8080, "localhost", () => console.log("start"));
+};
 
-server.listen(8080, "localhost", () => {
-  console.log("Server started at http://localhost:8080");
-});
+bootstrap();
--- src/schema.ts
+++ src/schema.ts
@@ -1,83 +1,18 @@
 import {
-  GraphQLID,
-  GraphQLInt,
   GraphQLNonNull,
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
 } from "graphql";
 
-const User = new GraphQLObjectType({
-  name: "User",
-  fields: () => ({
-    id: {
-      description: "user id",
-      type: new GraphQLNonNull(GraphQLID),
-    },
-    name: {
-      description: "user name",
-      type: new GraphQLNonNull(GraphQLString),
-      resolve: (parrent, arg, context)=> {
-        console.log(context)
-
-        return parrent.name
-      }
-    },
-    age: {
-      description: "user age",
-      type: new GraphQLNonNull(GraphQLInt),
-      resolve: () => 27,
-    },
-  }),
-});
-
-const users = [
-  { id: "123", name: "Ivan" },
-  { id: "456", name: "Oleg" },
-];
-
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: "Query",
     fields: () => ({
-      hello: {
+      sayHello: {
         type: new GraphQLNonNull(GraphQLString),
-        args: {
-          word: {
-            type: new GraphQLNonNull(GraphQLString),
-          },
-        },
-        resolve: (_p, args) => {
-          const { word } = args;
-
-          return `hello ${word}`;
-        },
+        resolve: () => "hello",
       },
-      user: {
-        type: User,
-        args: {
-          userId: {
-            type: new GraphQLNonNull(GraphQLID),
-          },
-        },
-        // type: new GraphQLNonNull(User),
-        resolve: (_p, args) => {
-          const { userId } = args;
-          const user = users.find(({ id }) => {
-            return id === userId;
-          });
-          return user || null;
-        },
-      },
-      // пользователь: {
-      //   тип будет возвращен: '<User>',
-      //   // возвращаемое значение: () => {
-      //   //   return {
-      //   //     id: '12',
-      //   //     name: 'ivan'
-      //   //   }
-      //   // }
-      // }
     }),
   }),
 });
